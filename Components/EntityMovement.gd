4cd0cac526fca8f2a3d61f0ced8b57449b807717
LesterCantor <c012718j@student.staffs.ac.uk>
2024-08-06 17:43:31 +0100

    movement components added and test world collisions set up.

---
+++ Components/EntityMovement.gd
@@ -0,0 +1,15 @@
+class_name EntityMovement
+extends Node
+
+@export var actor: CharacterBody2D
+# Slide angle is mainly for platformers
+@export var min_slide_angle: float = 0.0
+
+func _ready() -> void:
+	actor.wall_min_slide_angle = min_slide_angle
+	# Motion mode is mainly for platformers 
+	# And floating helps for top down perspective
+	actor.motion_mode = actor.MOTION_MODE_FLOATING
+	
+func _physics_process(_delta: float) -> void:
+	actor.move_and_slide()
---
+++ Resources/EntityMovementStats.gd
@@ -0,0 +1,6 @@
+class_name EntityMovementStats
+extends Resource
+
+@export var max_speed: float = 100
+@export var acceleration: float = 1000
+@export var friction: float = 700
