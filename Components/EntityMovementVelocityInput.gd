e1ac17196dcac4006d676ac7a3bc5d0b7e58ffd1
LesterCantor <c012718j@student.staffs.ac.uk>
2024-08-07 01:21:31 +0100

    made input global variable in input component for access

--- Components/EntityMovementVelocityInput.gd
+++ Components/EntityMovementVelocityInput.gd
@@ -3,12 +3,13 @@ extends Node
 
 @export var actor: CharacterBody2D
 @export var movement_stats: EntityMovementStats
+var input: Vector2
 
 func _physics_process(delta: float) -> void:
 	# Get vectors depending on what input is pressed
-	var input = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	input = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
 	
 	if input.length() > 0:
-		actor.velocity = actor.velocity.move_toward(input * movement_stats.max_speed, movement_stats.acceleration * delta)
+		actor.velocity = actor.velocity.move_toward(input.normalized() * movement_stats.max_speed, movement_stats.acceleration * delta)
 	else:
 		actor.velocity = actor.velocity.move_toward(Vector2.ZERO, movement_stats.friction * delta)
---
+++ Player/player.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var entity_movement_input: EntityMovementVelocityInput = $EntityMovementVelocityInput
+@onready var entity_movement: EntityMovement = $EntityMovement
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+	# animation_tree.set("animation blend", entity_movement_input.input)
+	# add animations here based on player input 
